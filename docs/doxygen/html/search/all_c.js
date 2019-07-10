6631275c7f1fa81a4b1fa1751827070f475d9d7f
benagin <smithbr95@gmail.com>
2019-07-09 23:20:56 -0700

    Update docs

--- docs/doxygen/html/search/all_c.js
+++ docs/doxygen/html/search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parse',['parse',['../namespacebstd_1_1json_1_1parser.html#a83db42618d8826c853638da0cb4e4b61',1,'bstd::json::parser::parse(const std::string &amp;_string)'],['../namespacebstd_1_1json_1_1parser.html#a36e397bbe297263d56b6164d7a93d0bf',1,'bstd::json::parser::parse(const lexer &amp;_lexer)']]],
+  ['parse',['parse',['../namespacebstd_1_1json_1_1parser.html#a8c8deae29dda22ea0212d292ff37e45b',1,'bstd::json::parser::parse(const char *_string)'],['../namespacebstd_1_1json_1_1parser.html#a83db42618d8826c853638da0cb4e4b61',1,'bstd::json::parser::parse(const std::string &amp;_string)'],['../namespacebstd_1_1json_1_1parser.html#a36e397bbe297263d56b6164d7a93d0bf',1,'bstd::json::parser::parse(const lexer &amp;_lexer)']]],
   ['parse_5ferror',['parse_error',['../classbstd_1_1json_1_1parser_1_1parse__error.html',1,'bstd::json::parser::parse_error'],['../classbstd_1_1json_1_1parser_1_1parse__error.html#a19e8d647196cfdde9af289041aab7df9',1,'bstd::json::parser::parse_error::parse_error()']]],
   ['parse_5ferror_2ehpp',['parse_error.hpp',['../parse__error_8hpp.html',1,'']]],
   ['parser_2ecpp',['parser.cpp',['../parser_8cpp.html',1,'']]],
--- docs/doxygen/html/search/functions_4.js
+++ docs/doxygen/html/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['lex',['lex',['../classbstd_1_1json_1_1parser_1_1lexer.html#ac93c684fd3c72ae1f3fa5d1c961ebf79',1,'bstd::json::parser::lexer']]],
-  ['lexer',['lexer',['../classbstd_1_1json_1_1parser_1_1lexer.html#ab5642637058a695495e40411d44855f5',1,'bstd::json::parser::lexer::lexer()'],['../classbstd_1_1json_1_1parser_1_1lexer.html#ac6487641b401b0f88cc90479af4f06a8',1,'bstd::json::parser::lexer::lexer(const lexer &amp;)=delete']]]
+  ['lexer',['lexer',['../classbstd_1_1json_1_1parser_1_1lexer.html#af39f7183bdf76f96ab2a0deaff7c3792',1,'bstd::json::parser::lexer::lexer(const bool _debug=false)'],['../classbstd_1_1json_1_1parser_1_1lexer.html#ac6487641b401b0f88cc90479af4f06a8',1,'bstd::json::parser::lexer::lexer(const lexer &amp;)=delete']]]
 ];

084217ed384e223e72ba2d4bbb71d2bc37a17009
benagin <smithbr95@gmail.com>
2019-08-13 23:10:35 -0700

    Update docs.

--- docs/doxygen/html/search/functions_c.js
+++ docs/doxygen/html/search/functions_c.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['value',['value',['../classbstd_1_1json_1_1structures_1_1value.html#ace0242e7c0b0796d7b4f9060621e23dd',1,'bstd::json::structures::value']]]
+  ['value',['value',['../classbstd_1_1json_1_1structures_1_1value.html#a374fb2c4268946d6d3aae066f90640b6',1,'bstd::json::structures::value::value(const std::string &amp;_string, const bool _debug=false)'],['../classbstd_1_1json_1_1structures_1_1value.html#ace0242e7c0b0796d7b4f9060621e23dd',1,'bstd::json::structures::value::value(const bool _debug)']]]
 ];
